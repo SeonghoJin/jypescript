d071d42568756df55acbd44c43e938844be5c0e2
SeonghoJin <seongho4996@naver.com>
2021-05-28 21:19:51 +0900

    코드 정리

--- src/Ioc/decorator/Autowired.ts
+++ src/Ioc/decorator/Autowired.ts
@@ -1,15 +1,30 @@
 import "reflect-metadata"
-import { Clazz, TypeGuard } from "../../interface/index.js";
-import { Container, Injector } from "../index.js";
-export const AutoWired = <T>(selectType?: Clazz | TypeGuard<T>) => {
+import { Clazz, TypeGuard } from "../../interface";
+import { Container, inject } from "../";
 
-    return (target: any, propertyKey: string) => {
-        let type = selectType !== undefined ? selectType : Reflect.getMetadata('design:type', target, propertyKey);
-        if (type === Object) return;
-        Injector(type)
-        const instance = Container.get(type);
-        Reflect.defineProperty(target, propertyKey, {
-            value: instance
-        });
+interface IAutoWiredArgs<T>{
+    class? : Clazz;
+    typeGuard? : TypeGuard<T>;
+}
+
+export const AutoWired = <T>(autoWiredArgs? : IAutoWiredArgs<T> | undefined) => {
+
+    return (target: any, propertyKey: string)=> {
+        if(autoWiredArgs === undefined){
+            const type = Reflect.getMetadata('design:type', target, propertyKey);
+            if(type === Object)throw Error("is this interface? insert TypeGuard");
+            Reflect.defineProperty(target, propertyKey, {
+                value: inject(type)
+            });
+        }
+        else if(autoWiredArgs.class !== undefined){
+            const type = autoWiredArgs.class;
+            Reflect.defineProperty(target, propertyKey, {
+                value: inject(type)
+            });
+        }
+        else if(autoWiredArgs.typeGuard !== undefined){
+            throw new Error("is this TypeGuard");
+        }
     }
 }
