9e0b9b93c471a567a1093c7098d5ea2d2b7a1b0a
moretall <seongho4996@naver.com>
2021-06-28 18:06:21 +0900

    refactoring

--- src/Ioc/Container.ts
+++ src/Ioc/Container.ts
@@ -1,31 +1,37 @@
 import { Clazz, TypeGuard } from "../interface";
 
 export const Container = new class {
+
     container: Map<Clazz, any>;
+
     constructor() {
         this.container = new Map<Clazz, any>();
     }
 
+    createInstance(clazz: Clazz, ...args: any[]) {
+        return new clazz(...args);
+    }
+
     set(clazz: Clazz, ...args: any[]) {
         let item = this.container.get(clazz);
-        if (item !== undefined) {
+        if (item != null) {
             return item;
         }
-        item = new clazz(...args);
+        item = this.createInstance(clazz, args);
         this.container.set(clazz, item);
         return item;
     }
 
     get(clazz: Clazz, ...args: any[]): Clazz | null {
         let item = this.container.get(clazz);
-        if (item === undefined) {
-            item = new clazz(...args);
+        if (item == null) {
+            item = this.createInstance(clazz, args);
             this.container.set(clazz, item);
         }
         return item;
     }
 
-    getBeanOfInterface<T>(typeGuard: TypeGuard<T>): T | null {
+    getBean<T>(typeGuard: TypeGuard<T>): T | null {
         let item: T | null = null;
 
         this.container.forEach((value, key) => {
